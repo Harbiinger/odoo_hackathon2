46fdb81f77d44b5da9194c0d7bfa206232d04b48
LicorneRose765 <210376@umons.ac.be>
2023-11-26 10:31:09 +0100

    backtoera win

--- app/src/main/java/officeOlympics/frontend/Controllers/winController.java
+++ app/src/main/java/officeOlympics/frontend/Controllers/winController.java
@@ -1,10 +1,18 @@
 package officeOlympics.frontend.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import officeOlympics.App;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class winController {
 
@@ -13,6 +21,15 @@ public class winController {
 
     public void initialize() {
         winImage.setImage(new javafx.scene.image.Image("/front/winBackground.png"));
+    }
+
+    public void backToEra(MouseEvent actionEvent) throws IOException {
+        URL url = App.class.getResource("/front/eraSelection.fxml");
+        Pane pane = FXMLLoader.load(url);
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
