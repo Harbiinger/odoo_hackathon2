6eb1a04c895a9a9d6e5c0b90105f8063495d7d92
LicorneRose765 <210376@umons.ac.be>
2023-11-26 09:51:27 +0100

    Come back to era from loosing

--- app/src/main/java/officeOlympics/frontend/Controllers/loseController.java
+++ app/src/main/java/officeOlympics/frontend/Controllers/loseController.java
@@ -1,10 +1,18 @@
 package officeOlympics.frontend.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import officeOlympics.App;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class loseController {
 
@@ -14,5 +22,15 @@ public class loseController {
     public void initialize() {
         loseImage.setImage(new javafx.scene.image.Image("/front/loseBackground.png"));
     }
+
+    public void backToEra(MouseEvent actionEvent) throws IOException {
+        URL url = App.class.getResource("/front/eraSelection.fxml");
+        Pane pane = FXMLLoader.load(url);
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 
 }
